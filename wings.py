a52e2fdb37170b7b684b63b9460b9eea6376796f
panghuuuuu <ysabella.panghulan@obf.ateneo.edu>
2022-11-27 22:33:09 +0800

    initial

--- wings.py
+++ wings.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def arrange_wings(wings, obs):
-    '''
-    Given the list of wings `wings` and observations `obs`, returns *any*
-    possible ordering of the wings (as a list) as determined by the observations,
-    or `None` if no such ordering is possible.
-    '''
-    # TO-DO: do stuff here
-    raise NotImplementedError
+    wingsLen = len(wings)
+    wingsDegree = {}
+    wingsObs = dict(obs)
+    wingsObs = {n:[v for u,v in obs if u == n] for n in wings}
+    wingsDegree = {i:0 for i in wings}
+    
+    for i in wingsObs:
+        for j in wingsObs[i]:
+            wingsDegree[j] += 1
+    sorted = [i for i in wingsObs if wingsDegree[i] == 0]
+    
+    for k in sorted:
+        for l in wingsObs[k]:
+            wingsDegree[l] -= 1
+            if(wingsDegree[l] == 0):
+                sorted.append(l)
+    if ((len(sorted) == wingsLen)): return sorted
+    else: return None
+
 
 
 ### DON'T touch anything below this line

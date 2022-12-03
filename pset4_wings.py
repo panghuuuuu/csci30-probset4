59a9373273ca5ae95df27b86dd88eb6fcb1e1d07
panghuuuuu <ysabella.panghulan@obf.ateneo.edu>
2022-12-03 08:20:20 +0800

    final

--- pset4_wings.py
+++ pset4_wings.py
@@ -1,27 +1,38 @@
 #!/usr/bin/env python3
 
 def arrange_wings(wings, obs):
-    wingsLen = len(wings)
-    wingsDegree = {}
-    wingsObs = dict(obs)
-    wingsObs = {n:[v for u,v in obs if u == n] for n in wings}
-    print(wingsObs)
-    wingsDegree = {i:0 for i in wings}
+    graph = {}
+    for u, v in obs:
+        graph.setdefault(u, []).append(v)
+    for i in wings:
+        if i not in list(graph.keys()):
+            graph[i] = []
     
-    for i in wingsObs:
-        for j in wingsObs[i]:
-            wingsDegree[j] += 1
-    sorted = [i for i in wingsObs if wingsDegree[i] == 0]
+    visited = {v:0 for v in graph}
+    result = []
+    cycle = [False]
+    for v in graph:
+        if visited[v] == 0:
+            dfs(graph,v,visited,result,cycle)
     
-    for k in sorted:
-        for l in wingsObs[k]:
-            wingsDegree[l] -= 1
-            if(wingsDegree[l] == 0):
-                sorted.append(l)
-    if ((len(sorted) == wingsLen)): return sorted
-    else: return None
+    if (cycle[0]):
+        return None
+    else:        
+       return (result[::-1])
 
-
+def dfs(graph,current,visited,result,cycle):
+    visited[current] = 1
+    for v in graph[current]:
+        if (visited[v] == 2):
+            continue
+        elif (visited[v] == 1):
+            #print("CYCLE!")
+            cycle[0] = True
+            return
+        else:
+            dfs(graph,v,visited,result,cycle)
+    visited[current] = 2
+    result.append(current)
 
 ### DON'T touch anything below this line
 #   this already takes care of the input and output
